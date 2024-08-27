b9cd2db7e5212dc057918de181e54f8873b80e35
jeffrepo <jeffer.leo93@gmail.com>
2024-08-27 09:56:16 -0600

    Update

---
+++ __manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock MX",
+
+    'summary': """ Desarrollos de inventarios para MX """,
+
+    'description': """
+        Desarrollos extras para inventarios
+    """,
+
+    'author': "JS",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '1.01',
+
+    'depends': ['l10n_mx_edi_stock'],
+
+    'data': [
+        'data/paperformat_ticket.xml',
+        'views/recibo_entrega.xml',
+    ],
+    'license': 'LGPL-3',
+}
